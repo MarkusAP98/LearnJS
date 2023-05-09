c7e110a018e0c15fb0e21cb49daa0bef41598002
Markus Phantha <markus.a.phantha@gmail.com>
2023-05-09 12:21:34 +0300

    got the to-do list updated!

--- to-do-list/tdo.js
+++ to-do-list/tdo.js
@@ -1,11 +1,21 @@
-var userInput = document.getElementById('userInput').value;
-const addBtn = document.getElementById('add-btn')
-const para = document.createElement('p')
+const myDivList = document.getElementById('myDiv-list')
+  // get the input
+  let userInput = document.getElementById('userInput') 
 
 
-
-function add() {
-  console.log(userInput);
-  para.textContent = userInput
-  document.body.querySelector('#myDiv-list').appendChild(para)
+  // this function user can add what they want to do into the list
+  function add() {  
+    // crates new 'p' element
+  const para = document.createElement('p') 
+    //crates a button and set the value to done
+  const doneBtn = document.createElement('button')  
+  doneBtn.textContent = 'Done!'
+  doneBtn.className = 'doneBtn'
+  // set the userInput value to paratextContent
+  para.textContent = userInput.value 
+  // making the new 'p' content
+  myDivList.appendChild(para) 
+  para.appendChild(doneBtn)
+  userInput.value = ''
 }
+
